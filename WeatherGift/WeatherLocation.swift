c9d922eea37e3593a7b23d11d6f7e555c61cc6ac
Michael Ryan <ryanabu@bc.edu>
2020-10-06 10:40:35 -0400

    Ch. 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Michael Ryan on 10/6/20.
+//  Copyright © 2020 Michael Ryan. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double 
+    
+}
