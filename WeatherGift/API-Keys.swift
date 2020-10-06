7d3ba7290569396b3c3425ccd2c0c1b6065955cd
Michael Ryan <ryanabu@bc.edu>
2020-10-06 11:30:20 -0400

    Ch. 6.4 with .gitignore

---
+++ WeatherGift/API-Keys.swift
@@ -0,0 +1,13 @@
+//
+//  API-Keys.swift
+//  WeatherGift
+//
+//  Created by Michael Ryan on 10/6/20.
+//  Copyright © 2020 Michael Ryan. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyAnRuYOJKvHJrNH4EzqnpriTXW_E4iN4OY"
+}
